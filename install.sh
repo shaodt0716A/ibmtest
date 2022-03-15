315a9a1dddbd7e4b0f7b33a23069b45bbedc8fff
shaodt0716A <70965053+shaodt0716A@users.noreply.github.com>
2022-03-15 10:42:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ create_mainfest_file(){
     read -p "请输入你的密码：" IBM_Passwd
     echo "用户密码：${IBM_Passwd}"
     ibmcloud login -a "https://cloud.ibm.com" -r "eu-gb" -u "${IBM_User_NAME}" -p "${IBM_Passwd}"
+    ibmloud target -g Default 
     read -p "请输入你的应用名称：" IBM_APP_NAME
     echo "应用名称：${IBM_APP_NAME}"
     read -p "请输入你的运行环境：" IBM_APP_NUM
